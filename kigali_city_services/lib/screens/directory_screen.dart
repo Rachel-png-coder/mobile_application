38f4f3db0b2f433b9a19915c419c8f691ddab732
Rachel Toronga <r.toronga@alustudent.com>
2026-03-09 23:07:43 +0200

    added directory file

--- kigali_city_services/lib/screens/directory_screen.dart
+++ kigali_city_services/lib/screens/directory_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import '../providers/listing_provider.dart';
 import '../providers/auth_provider.dart';
 import '../widgets/listing_card.dart';
